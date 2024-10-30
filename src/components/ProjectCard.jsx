5e7bd305d6ef219f183a05e86806bcfef53d0aa5
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-10-30 17:24:30 +0100

    added aiimggenerator project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,12 +7,12 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
+      <h3 className={styles.title}>{title}</h3>
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={styles.img}
       />
-      <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
         {skills.map((skill, id) => {
