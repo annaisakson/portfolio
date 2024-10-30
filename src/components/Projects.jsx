1867ddd60ef31c6327c2e17d3285586b2e77ef06
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-10-30 19:00:13 +0100

    small change to text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>My projects</h2>
+      <h2 className={styles.title}>My latest projects</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
