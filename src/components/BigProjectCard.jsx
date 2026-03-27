f44384474ab5e407ec0f89efb49bdacb073071d4
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2026-03-27 17:29:36 +0100

    design on cards

--- src/components/BigProjectCard.jsx
+++ src/components/BigProjectCard.jsx
@@ -1,41 +1,66 @@
+import { useState } from "react";
 import styles from "../CSS/BigProjectCard.module.css";
 import { getImageUrl } from "../utils";
+import { PiArrowArcRightBold } from "react-icons/pi";
 
 export const BigProjectCard = ({
-  project: { title, subheading, imageSrc, description, list, skills },
+  project: { title, subheading, imageSrc, description, list, skills, logo },
 }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
     <>
-      <section className={styles.container}>
-        <img
-          src={getImageUrl(imageSrc)}
-          alt={`Image of ${title}`}
-          className={styles.img}
-        />
-        <div className={styles.content}>
-          <div>
-            <h2 className={styles.title}>{title}</h2>
-            <p className={styles.subheading}>{subheading}</p>
+      <section
+        className={styles.container}
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <div
+          className={`${styles.flipWrapper} ${isFlipped ? styles.flipped : ""}`}
+        >
+          <div className={styles.cardFront}>
+            <img
+              src={getImageUrl(imageSrc)}
+              alt={`Image of ${title}`}
+              className={styles.img}
+            />
+            <div className={styles.content}>
+              <div>
+                <h2 className={styles.title}>{title}</h2>
+                <p className={styles.subheading}>{subheading}</p>
+              </div>
+              <ul className={styles.skillsList}>
+                {skills.map((skill, id) => (
+                  <li key={id} className={styles.skill}>
+                    {skill}
+                  </li>
+                ))}
+              </ul>{" "}
+              <PiArrowArcRightBold className={styles.arrow} />
+            </div>
           </div>
-          <p className={styles.description}>{description}</p>
-          <ul className={styles.list}>
-            {list.map((item, id) => {
-              return (
-                <li key={id} item={item} className={styles.listItem}>
-                  {item}
-                </li>
-              );
-            })}
-          </ul>
-          <ul className={styles.skillsList}>
-            {skills.map((skill, id) => {
-              return (
-                <li key={id} className={styles.skill}>
-                  {skill}
-                </li>
-              );
-            })}
-          </ul>
+
+          <div className={styles.cardBack}>
+            <img
+              src={getImageUrl(imageSrc)}
+              alt={`Image of ${title}`}
+              className={styles.img}
+            />
+            <div className={styles.content}>
+              <img
+                src={getImageUrl(logo)}
+                alt={`Image of ${title}`}
+                className={styles.logo}
+              />
+              <p className={styles.description}>{description}</p>
+              <ul className={styles.list}>
+                {list.map((item, id) => (
+                  <li key={id} className={styles.listItem}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     </>
